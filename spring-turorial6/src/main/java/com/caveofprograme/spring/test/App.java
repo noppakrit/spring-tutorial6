4ab08e0608d5924394d4958b1aaaf6d59a410301
noppakrit <noppakrit@192.168.110.129>
2015-08-07 15:29:51 +0700

    init

--- spring-turorial6/src/main/java/com/caveofprograme/spring/test/App.java
+++ spring-turorial6/src/main/java/com/caveofprograme/spring/test/App.java
@@ -8,10 +8,11 @@ public class App {
 
 	public static void main(String[] args) {
 		
-		ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
-		
+		ApplicationContext context = new FileSystemXmlApplicationContext("src/main/java/com/caveofprograme/spring/test/beans/beans.xml");
 		Person person = (Person) context.getBean("person");
 		person.speak();
+		
+		((FileSystemXmlApplicationContext)context).close();
 
 	}
 
