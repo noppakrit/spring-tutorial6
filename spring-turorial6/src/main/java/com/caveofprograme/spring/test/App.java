30759687c8536e20039a84f24128ed3eba2c75b8
noppakrit <noppakrit@192.168.110.129>
2015-08-07 14:54:38 +0700

    Init

---
+++ spring-turorial6/src/main/java/com/caveofprograme/spring/test/App.java
@@ -0,0 +1,18 @@
+package com.caveofprograme.spring.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
+		
+		Person person = (Person) context.getBean("person");
+		person.speak();
+
+	}
+
+}
